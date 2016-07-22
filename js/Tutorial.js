8c7aa452139b8a71797cb42c5ba6776063db1f7a
alex <horatioesf@virginmedia.com>
2016-07-22 20:09:11 +0100

    fixed some stuff

--- js/Tutorial.js
+++ js/Tutorial.js
@@ -36,13 +36,14 @@ Game.Tutorial.prototype = {
 				var enemy = enemies[enemyKey];
 				updtEnemyMovement(game, enemy);
 			}
-		} else if (drainingplayer && ectoplasm >0){
+		} else if (drainingplayer && ectoplasm >0 && jumpTimer < game.time.now){
+			jumpTimer = game.time.now + 50
 			ectoplasm -=1
 			ectoplasm_text.text = ': ' + ectoplasm.toString() + '/15';
 		} else if (protoplayer.visible && enter_underworld.visible){
 			controls.right.onUp.add(function(){
 				enter_underworld.destroy();
-				portal = game.add.sprite(protoplayer.x - 140, protoplayer.y - 75, 'portal');
+				portal = game.add.sprite(protoplayer.x + 80, protoplayer.y - 75, 'portal');
 				portal_open = portal.animations.add('main', [0, 1, 2, 3, 4, 5, 6, 7, 8], 9, false);
 				drainingplayer = true;
 				portal_open.onComplete.add(function(){
@@ -62,7 +63,7 @@ Game.Tutorial.prototype = {
 			//protoplayer.destroy();
 			//initPlayer(game, 374, 430)
 
-			enter_underworld = game.add.text(protoplayer.x + 80, protoplayer.y - 20, ' Press D to enter\n the underworld!', {fontSize: '32px', fill: '#fff'});
+			enter_underworld = game.add.text(protoplayer.x - 300, protoplayer.y - 40, ' Press D to enter\n the underworld!', {fontSize: '32px', fill: '#fff'});
 
 		}, this);
 		protoplayer.animations.play('appearFromGrave');
